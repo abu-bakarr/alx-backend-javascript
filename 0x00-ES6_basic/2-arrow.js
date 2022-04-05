ac329e8fb19c3d15dde22a2d1fb21195ec95da5f
abu-bakarr <abangura14@yahoo.com>
2022-04-05 13:34:51 +0000

    Add ex 3

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+export default function getNeighborhoodsList() {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+    const self = this;
+    this.getNeighborhood = add(newNeighborhood)
+}
+const add = (data) => {
+    self.sanFranciscoNeighborhoods.push(data);
+    return self.sanFranciscoNeighborhoods;
+}
