0d1f6cf9d4667104fb526b6127ad8a067ec77c2d
abu-bakarr <abangura14@yahoo.com>
2022-04-06 05:06:44 +0000

    Add exer 3

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,17 @@
+function handleResponseFromAPI(promise) {
+    return new Promise(async(resolve, reject) => {
+            promise && await resolve({ status: 200, body: 'Success' });
+            await reject('The fake API is not working currently')
+        })
+        .then((success) => {
+            console.log(success);
+        })
+        .catch((error) => {
+            console.log("error", error);
+        })
+        .finally(() => {
+            console.log("Got a response from the API");
+        })
+}
+
+export default handleResponseFromAPI;
