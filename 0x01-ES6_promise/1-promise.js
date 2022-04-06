186e2ccc61ec2a87889362a37e747f2438917aa2
abu-bakarr <abangura14@yahoo.com>
2022-04-06 13:24:27 +0000

    Fix

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 function getFullResponseFromAPI(success) {
     return new Promise(async(resolve, reject) => {
-            success === true && await resolve({ status: 200, body: 'Success' });
-            success === false && await reject('The fake API is not working currently')
+            success && await resolve({ status: 200, body: 'Success' });
+            await reject('The fake API is not working currently')
         })
         .then((success) => {
             console.log(success);
