20814cc110d404324172514fea82de3c511f4dd7
abu-bakarr <abangura14@yahoo.com>
2022-04-06 14:28:38 +0000

    Fix

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,3 @@
 export default function getResponseFromAPI() {
-    return Promise.resolve('foo')
+  return Promise.resolve('foo');
 }
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,11 @@
-import { createUser, uploadPhoto } from './utils'
-
+import { createUser, uploadPhoto } from './utils';
 
 function handleProfileSignup() {
-    var resolvedPromisesArray = [uploadPhoto(), createUser()];
+    const resolvedPromisesArray = [uploadPhoto(), createUser()];
     Promise.all(resolvedPromisesArray)
         .then((success) => {
             console.log(success[0].body, success[1].firstName, success[1].lastName);
-        })
+        });
 }
 
 export default handleProfileSignup;
