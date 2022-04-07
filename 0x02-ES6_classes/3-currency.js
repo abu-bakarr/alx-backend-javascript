21efa3b24cf7760fa4bcc3aae89b1ee35f4f2beb
abu-bakarr <abangura14@yahoo.com>
2022-04-07 12:41:01 +0000

    Add exer 4

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code === 'string') this._code = code;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
+
+export default Currency;
