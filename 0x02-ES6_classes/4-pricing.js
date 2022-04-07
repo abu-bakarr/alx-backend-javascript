835804279101bab97857707ef5bea27b89e632dd
abu-bakarr <abangura14@yahoo.com>
2022-04-07 12:51:11 +0000

    fix

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,10 +1,17 @@
 import Currency from './3-currency';
 
-class Pricing {
+export default class Pricing {
   constructor(amount, currency) {
-    this.amount = amount;
-    this.currency = currency;
+    /* eslint-disable no-underscore-dangle */
+    this._amount = amount;
+    this._currency = currency;
+    /* eslint-disable no-underscore-dangle */
   }
+
+  get amount() {
+    return this._amount;
+  }
+
   set amount(amount) {
     if (typeof amount === 'number') this._amount = amount;
   }
@@ -17,17 +24,11 @@ class Pricing {
     if (currency instanceof Currency) this._currency = currency;
   }
 
-  get amount() {
-    return this._amount;
-  }
-
   displayFullPrice() {
-    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
 }
-
-export default Pricing;
