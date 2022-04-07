1547081dfb10a265a43e5449ad0715510f467477
abu-bakarr <abangura14@yahoo.com>
2022-04-07 12:37:16 +0000

    add exer 2

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,7 +1,5 @@
 export default class ClassRoom {
   constructor(maxStudentsSize) {
-    /* eslint-disable no-underscore-dangle */
     this._maxStudentsSize = maxStudentsSize;
-    /* eslint-disable no-underscore-dangle */
   }
 }
